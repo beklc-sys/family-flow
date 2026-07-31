ef31b238fdb26647663834c979848da320bf3dd6
beklc-sys <christoph.beklas@gmail.com>
2026-07-31 13:31:01 +0200

    Bump service worker cache to v33

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = "family-flow-v32";
+const CACHE_NAME = "family-flow-v33";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=32",
-  "./favorites.css?v=32",
+  "./style.css?v=33",
+  "./favorites.css?v=33",
   "./manifest.webmanifest",
   "./js/config.js",
   "./js/supabase.js",
   "./js/store.js",
-  "./js/app.js?v=32",
+  "./js/app.js?v=33",
   "./js/shopping.js",
   "./js/favorites.js",
   "./js/ui.js",
